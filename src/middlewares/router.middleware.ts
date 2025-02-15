02926f83abba6345ba170992d995c9ff772665ea
Volkswg <volkswg@gmail.com>
2025-02-15 18:03:07 +0700

    feat: [BE] add auth checking on router middleware

--- src/middlewares/router.middleware.ts
+++ src/middlewares/router.middleware.ts
@@ -1,13 +1,31 @@
 import { type NextRequest } from 'next/server';
 import RequestMiddleware, { ProcessedParams } from './request.middleware';
+import { getCookies } from '@/services/cookies.service';
+import { verifyAccessToken } from '@/services/auth-token.service';
 
 export const withMiddleware = (
-  handler: (params: ProcessedParams) => unknown
+  handler: (params: ProcessedParams) => unknown,
+  needAuth: boolean = false
 ) => {
   return async (
     req: NextRequest,
     params: { params: Promise<Record<string, string>> }
   ) => {
+    // auth section
+    if (needAuth) {
+      const accessTokenObj = getCookies(req.cookies, 'accessToken');
+      if (!accessTokenObj) {
+        return Response.redirect(new URL('/login', req.url));
+      }
+      const { value: accessToken } = accessTokenObj;
+      try {
+        await verifyAccessToken(accessToken);
+      } catch (err) {
+        console.log(err);
+        return Response.redirect(new URL('/login', req.url));
+      }
+    }
+
     const requestMiddleware = new RequestMiddleware(req, params);
     const processedParams = await requestMiddleware.getRequestParams();
     try {
