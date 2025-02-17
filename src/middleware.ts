b2f4ddb9c54727db487ce04984f93066d8c19158
Volkswg <volkswg@gmail.com>
2025-02-18 04:47:29 +0700

    refactor: [FE] update page middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,15 @@
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
 
+const redirectPageHandler = (req: NextRequest) => {
+  const accessTokenObj = req.cookies.get('accessToken');
+  if (!accessTokenObj) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  } else {
+    return NextResponse.redirect(new URL('/data-mapping', req.url));
+  }
+};
+
 const guardPageHandler = (req: NextRequest) => {
   const accessTokenObj = req.cookies.get('accessToken');
   if (!accessTokenObj) {
@@ -21,6 +30,8 @@ export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
   if (pathname.startsWith('/login')) {
     return loginPageHandler(req);
+  } else if (pathname === '/') {
+    return redirectPageHandler(req);
   } else {
     return guardPageHandler(req);
   }
@@ -29,6 +40,4 @@ export function middleware(req: NextRequest) {
 export const config = {
   matcher:
     '/((?!api|static|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
-  // matcher:
-  //   '/((?!api|static|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|login).*)',
 };
